74a3aa7ddaa7532cca629fe912df43c2d1c61c2c
Ari Jain <thearijain@gmail.com>
2019-12-27 14:43:29 -0500

    Initial Commit

---
+++ CodeNotes2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeNotes2.0
+//
+//  Created by Ari Jain on 12/27/19.
+//  Copyright © 2019 Arihant Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
