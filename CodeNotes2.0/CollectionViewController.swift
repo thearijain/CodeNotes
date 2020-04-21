484273d5c7b7bf5c9699dbd623d85ef798d46ca8
Ari Jain <thearijain@gmail.com>
2020-04-21 00:05:44 -0400

    deleted the original view controller home screen - new home screen is the ui collection view controller - linked this with the overlay make new subject screen

--- CodeNotes2.0/CollectionViewController.swift
+++ CodeNotes2.0/CollectionViewController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+struct Notebook {
+    var notebookName = String?("")
+    var date = Date()
+}
+
+var arrayOfNotebooks: [Notebook] = []
+var setOfNotebookNames = Set<String>()
+
 class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet var CollectionViewOutlet: UICollectionView!
@@ -15,9 +23,21 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Sets the side inserts of the CollectionView
         let flow = CollectionViewOutlet.collectionViewLayout as! UICollectionViewFlowLayout
            flow.sectionInset = UIEdgeInsets(top: 50, left: 50, bottom: 50, right: 50)
            flow.minimumLineSpacing = 50
+        
+        //Creates a new Unfiled Notees notebook if needed
+        if (arrayOfNotebooks.count == 0) {
+            let newNotebook = Notebook(notebookName: "Unfiled Notes", date: Date())
+            arrayOfNotebooks.append(newNotebook)
+            setOfNotebookNames.insert("Unfiled Notes")
+        }
+        //Updates the UI
+        NotificationCenter.default.addObserver(forName: .updateInterface, object: nil, queue: OperationQueue.main) { (notification) in
+            self.updateUI()
+        }
         
     }
    
@@ -37,13 +57,19 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
     }
     
     
-    //Returns number of notebooks in the collectionView
+    //Returns number of notebooks to put into the collectionView
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 11
     }
     
+    //Returns the cell object built on the main storyboard
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath) as UICollectionViewCell
         return cell
+    }
+    
+    //Updates the UI of the homepage
+    func updateUI() {
+        print("updated")
     }
 }
